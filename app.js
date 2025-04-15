1f35e82442286bb0b9e71610927241fe1808b835
LoveKaushik2001 <lovekaushik2001@gmail.com>
2025-04-15 17:48:42 +0530

    lec-3

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement(
-    "div",
-    { id: "child1" },
-    [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ],
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ])
-  ),
-]);
-
 // const heading = React.createElement("h1", { id: "heading" }, "Hello world from React!");
+
+const HeadingComponent = () => {
+  return (
+    <h1 id="heading">Hello world from React!</h1>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
